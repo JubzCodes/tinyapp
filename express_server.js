406083d6fe1101c26818e11c789e228bf0717fef
jaden <jadenbusiness@hotmail.com>
2021-12-09 23:05:30 +0000

    registration errors/registering new users using a bunch of things from the lecture and mentors

--- express_server.js
+++ express_server.js
@@ -19,12 +19,36 @@ const users = {
     email: "user@example.com", 
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
     password: "dishwasher-funk"
   }
 };
+
+const generateRandomString = function() {
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }
+  return randomString;
+};
+
+const findUserByEmail = (email, users) => {
+  for (let userId in users) {
+    const user = users[userId]; // => retrieve the value
+
+    if (user.email === email) {
+      return user;
+    }
+  }
+
+  return false;
+};
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -39,15 +63,16 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  const user = users[req.cookies["user_id"]]
   const templateVars = { urls: urlDatabase,
-  username: req.cookies["username"] };
+  user };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const userId = req.cookies["user_id"];
   const templateVars = {
-  username: req.cookies["username"],
-};
+  username: req.cookies["username"], user: users[userId] };
 res.render("urls_new", templateVars);
 });
 
@@ -78,14 +103,24 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+const templateVars = { user: null };
+  res.render("register", templateVars);
 });
 
 app.post("/register", (req, res) => {
-const userId = Math.random().toString(36).substring(2, 8)
-const email = req.body.email;
-const password = req.body.password;
-const newUser = {
+
+  const userId = Math.random().toString(36).substring(2, 8)
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  const user = findUserByEmail(email, users);
+
+  if (user) {
+    res.status(403).send('Sorry, user already exists!');
+    return;
+  }
+
+  const newUser = {
     id: userId, 
     email, 
     password,
@@ -117,13 +152,3 @@ app.post("/logout", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-const generateRandomString = function() {
-  let randomString = "";
-  for (let i = 0; i < 6; i++) {
-    const randomCharCode = Math.floor(Math.random() * 26 + 97);
-    const randomChar = String.fromCharCode(randomCharCode);
-    randomString += randomChar;
-  }
-  return randomString;
-};
